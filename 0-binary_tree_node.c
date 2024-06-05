ca877dc3673938e02932db7a9bcfb40fc87f01c7
jeffrey7890 <d3v3loperpy@gmail.com>
2024-06-05 03:15:22 +0100

    changed UINT_MAX to INT_MAX

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (value > UINT_MAX)
+	if (value > INT_MAX)
 		return (NULL);
 
 	new_node->parent = parent;
